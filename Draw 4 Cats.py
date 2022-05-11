be7897ac6e515afb4c0d086a827774d933af3c40
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-11 12:09:01 -0700

    Add files via upload

---
+++ Draw 4 Cats.py
@@ -0,0 +1,31 @@
+"""
+LESSON: Sky Dodge Project
+WARMUP 1
+"""
+
+import tsk
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([1018, 573])
+background = tsk.Sprite("CatRoom.jpg", 0, 0)
+cats = []
+
+# Create a row of cats
+for i in range(4):
+    x = 50 + i * 250
+    cat = tsk.Sprite("Cat.png", x, 200)
+    cats.append(cat)
+    
+drawing = True
+while drawing:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+
+    background.draw()
+    for cat in cats:
+        cat.draw()
+
+    pygame.display.flip()
