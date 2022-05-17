5b3091b6f7421b474a83567552d4167f3b199dfd
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-17 12:59:53 -0700

    Update Sky Dodge Project.py

--- Sky Dodge Project.py
+++ Sky Dodge Project.py
@@ -4,13 +4,11 @@ LESSON: Sky Dodge Project
 
 #### ---- LIBRARIES ---- ####
 
-# E1: Import the random library
+# E1:
 import random
-
-# B1: Import the tsk library
+# B1:
 import tsk
-
-# A1-A2: Import and set up pygame
+# A1-A2:
 import pygame
 pygame.init()
 
@@ -18,330 +16,235 @@ pygame.init()
 
 # --- Program variables --- #
 
-# A3: Create a window of size [1018, 573]
-window = pygame.display.set_mode([1018, 573])
-
-# B2: Create a background sprite using "SkyScrolling.jpg"
-background = tsk.Sprite("SkyScrolling.jpg", 0, 0)
-
-# C1: Create a Clock
+# A3:
+w = pygame.display.set_mode([1018, 573])
+# B2:
+bg = tsk.Sprite("SkyScrolling.jpg", 0, 0)
+# C1:
 c = pygame.time.Clock()
 
 # --- Dragon variables --- #
 
-# D1-D2: Create a dragon sprite using the "DragonFlying.png"
-# spritesheet with 4 rows and 6 columns
+# D1-D2:
 drs = tsk.ImageSheet("DragonFlying.png", 4, 6)
 dragon = tsk.Sprite(drs, 0, 0)
+# D3:
+drg_speed = 0.3
 
-# D3: Create a speed variable for the dragon's movement
-dragonspeed = 0.3
 
 # --- Cloud variables --- #
 
-# E2-E3: Create an empty list to store clouds and a
-# list of cloud filenames containing "Cloud1.png",
-# "Cloud2.png" and "Cloud3.png"
+# E2-E3:
 clouds = []
-filenames = ["Cloud1.png", "Cloud2.png", "Cloud2.png"]
+cloud_fnms = ["Cloud1.png", "Cloud2.png", "Cloud3.png"]
+# E4-E5:
+cld_timer = 0
+cld_spn = 5000
+# F1:
+cld_speed = 0.1
 
-# E4-E5: Create variables for the total time between
-# cloud spawns and the time since the most recent spawn
-cloudspawns = 0
-recentspawn = 0
-
-# F1: Create a speed for the clouds that's less than
-# the dragon's speed
-cloudspeed = 0.1
 
 # --- Flame variables --- #
 
-# I1-I2: Create a flame sprite using the "FireBlast.png"
-# spritesheet with 4 rows and 6 columns. Start it
-# offscreen above the top of the window.
-flamesheet = tsk.ImageSheet("FireBlast.png", 4, 6)
-flame = tsk.Sprite(flamesheet, 0, 0)
-
-# I3: Create a variable to track whether the flame is
-# on or off
-flameonoroff = False
-
-# J1-J2: Create variables for the time since the flame
-# last turned on, and how long the flame should be on
-timesinceflameturnedon = 0
-howlongflameshouldbeon = 0
+# I1-I2:
+flm_sheet = tsk.ImageSheet("FireBlast.png", 4, 6)
+flame = tsk.Sprite(flm_sheet, 100, 100)
+# I3:
+flame_on = False
+# J1-J2:
+flm_on_timer = 0
+flm_on_regulator = 0
 
 # --- Balloon variables --- #
 
-# K1-K3: Create an empty list for the balloons and
-# variables for the time between balloon spawns and
-# the time since the last spawn
+# K1-K3:
 balloons = []
-timebetweenballoonspawns = 0
-timesincelastspawn = 0
+bln_timer = 0
+bln_spn = 7500
+# L1:
+bln_speed = 0.2
 
-# L1: Create a variable for the balloon's speed that's
-# faster than the clouds but slower than the dragon
-balloonspeed = 0.2
 
 # --- Score variables --- #
 
-# O1: Create a variable to track the distance traveled
-distancetravelled = 0
-
-# P1: Create a variable to track the score
+# O1:
+distance = 0
+# P1:
 score = 0
+
 
 #### ---- MAIN LOOP ---- ####
 
-# A4-A5: Create a main loop
+# A4-A5:
 running = True
 while running:
+    bln_timer += c.get_time()
+    cld_timer += c.get_time()
+    distance += c.get_time()
 
     #### ---- EVENT LOOP ---- ####
 
-    # A6-A8: Create an event loop
-    # ---> TEST AFTER THESE LINES <--- #
+    # A6-A8:
     for event in pygame.event.get():
+
+        # --- Quit --- #
         if event.type == pygame.QUIT:
             running = False
-            
+
         # --- Spacebar pressed --- #
 
-        # I4-I5: If the user hit spacebar, create a new
-        # fire sprite with the same variable and
-        # ImageSheet as the first one, and put it in
-        # front of the dragon
+        # I4-I5:
         if event.type == pygame.KEYDOWN:
+            # I6:
             if event.key == pygame.K_SPACE:
-                flameonoroff = True
-
+                flame_on = True
+            if event.key == pygame.K_DOWN and dragon.y < (593 - 130):
+                drg_speed = 10
+            if event.key == pygame.K_UP and dragon.y > 0:
+                drg_speed = -10
+        if event.type == pygame.KEYUP:
+            # I6:
+            if event.key == pygame.K_SPACE:
+                flame_on = True
+            drg_speed = 0
     #### ---- MOVE WITH ARROW KEYS ---- ####
-    
-    # D4-D7: If the up or down keys are held down and
-    # the dragon isn't off the screen, move the dragon.
-    # (Hint: use tsk.is_key_down())
-    if tsk.is_key_down(pygame.K_UP):
-        dragon.y -= dragonspeed
-    if tsk.is_key_down(pygame.K_DOWN):
-        dragon.y += dragonspeed
-
+   
+    # D4-D7:
+    dragon.y += drg_speed
+    flame.y += drg_speed
     #### ---- CHECK FOR COLLISIONS ---- ####
 
     # --- Dragon hits a cloud --- #
 
-    # H1-H4: Go through every cloud in the list and see
-    # if they collide with the dragon. If so, end the
-    # main loop and don't check any more collisions.
-    # ---> TEST AFTER THESE LINES <--- #
+    # H1-H4:
     for cloud in clouds:
         if pygame.sprite.collide_rect(dragon, cloud):
             running = False
             break
 
     # --- Flame hits a balloon --- #
-
-    # N1: Create a new list for balloons to remove
-
-
-    # N2-N4: Go through all the balloons and, if they
-    # collide with the flame, add them to the new list
-
-
-
-
-            # P2: Increment the score by 1
-
-
-    # N5-N6: Go through the new list and remove
-    # everything in it from the original balloons list
-    # ---> TEST AFTER THESE LINES <--- #
-
-
-
-
+    if flame_on:
+        for i in balloons:
+            if pygame.sprite.collide_rect(i, flame):
+                balloons.remove(i)
+                score += 1
     #### ---- SPAWN NEW SPRITES ---- ####
 
     # --- Spawn clouds --- #
 
-    # E6: Track time in the time since last cloud
-    # spawned variable
-
-
-    # E7-E10: If enough time has passed since the last
-    # cloud spawned that it's time to spawn a new one,
-    # create a new cloud sprite using a random image
-    # from the list of cloud filenames. Start it
-    # offscreen on the right at a random height.
-
-
-
-
-
-        # E11-E12: Put the cloud in the list of clouds
-        # and re-set the time since last spawn timer
-        # ---> TEST AFTER THESE LINES <--- #
-
-
-
-        # Q1: Decrease the time between cloud spawns by
-        # 50 milliseconds
-
-
+    # E6:
+    if cld_timer > cld_spn:
+    # E7-E10:
+        cloud = tsk.Sprite(cloud_fnms[random.randint(0, 2)], 0, 0)
+        cloud.x = 1018
+        cloud.y = random.randint(0, 573-115)
+        # E11-E12:
+        clouds.append(cloud)
+        # Q1:
+        cld_speed += 0.01
+        cld_timer = 0
 
     # --- Spawn balloons --- #
 
-    # K4: Track the time since the last balloon
-    # spawned.
-
-
-    # K5-K6: If enough time has passed that it's time
-    # to spawn a new balloon, create a new balloon
-    # sprite with the image "Balloon.png" offscreen on
-    # the right at a random height.
-
-
-
-        # K7-K8: Put the balloon in the list of
-        # balloons and re-set the time since last spawn
-        # ---> TEST AFTER THESE LINES <--- #
-
-
+    # K4:
+    if bln_timer > bln_spn:
+    # K5-K6:
+        bln_timer = 0
+        # K7-K8:
+        bln = tsk.Sprite("Balloon.png", 0, 0)
+        bln.y = random.randint(0, 573-164)
+        bln.x = 1018
+        balloons.append(bln)
 
 
     #### ---- STOP FLAME ---- ####
 
-    # J3-J4: If the flame is on, keep track of how long
-    # using the timer
-
-
-
-        # J5-J8: If the flame has been on long enough,
-        # set its status to off, re-set the timer, and
-        # move the flame above the top of the screen
-        # ---> TEST AFTER THESE LINES <--- #
-
-
-
-
-
+    # J3-J4:
+    if flame_on:
+        # J5-J8:
+        flm_on_regulator += c.get_time()
+        if flm_on_regulator > 500:
+            flm_on_regulator = 0
+            flame_on = False
 
     #### --- SCROLL SCREEN --- ####
 
     # --- Background --- #
 
-    # C2-C4: Move the background to the left. If its
-    # center reaches the left edge of the screen, set
-    # it back to the right edge.
-
-
-
-
+    # C2-C4:
+    if bg.center_x < 0:
+        bg.center_x = 1018
+    bg.center_x -= 0.1 * c.get_time()
+    bg.draw()
 
     # --- Clouds --- #
 
-    # F2-F3: Move every cloud in the list to the left
-
-
-
-
+    # F2-F3:
+    for i in clouds:
+        i.x -= cld_speed * c.get_time()
+        i.draw()
+        if i.x < -223:
+    # G2-G4:
+            clouds.remove(i)
     # --- Balloons --- #
 
-    # L2-L3: Move every balloon in the list to the left
-
-
+    # L2-L3:
+    for i in balloons:
+        i.x -= bln_speed * c.get_time()
 
 
     #### ---- REMOVE OFF-SCREEN SPRITES ---- ####
 
      # --- Clouds --- #
-
-    # G1: Create a list for the clouds to remove
-
-
-    # G2-G4: Go through every cloud in the clouds list
-    # and, if it's offscreen on the left, put it in the
-    # new list
-
-
-
-
-    # G5-G6: Go through the new list and remove
-    # everything in it from the original clouds list
-    # ---> TEST AFTER THESE LINES <--- #
+        i.draw()
+    # G1:
+        if i.x < -102:
+    # G2-G4:
+            balloons.remove(i)
+    # G5-G6:
 
 
 
     # --- Balloons --- #
 
-    # M1: Create an list for the balloons to remove
+    # M1:
 
+    # M2-M4:
 
-    # M2-M4: Go through every balloon in the balloon
-    # list and, if it's offscreen on the left, put it
-    # in the new list
-
-
-
-
-    # M5-M6: Go through the new list and remove
-    # everything in it from the original balloons list
-    # ---> TEST AFTER THESE LINES <--- #
-
-
+    # M5-M6:
 
 
     #### ---- DRAW FRAME ---- ####
+    dragon.update(c.get_time())
+    flame.update(c.get_time())
+    # B3:
+   
+    # F4-F5:
+   
+    # L4-L5:
 
-    # B3: Draw the background
-    background.draw()
-
-    # F4-F5: Draw every cloud in the list
-    # ---> TEST AFTER THESE LINES <--- #
-
-
-
-    # L4-L5: Draw every balloon in the list
-    # ---> TEST AFTER THESE LINES <--- #
-
-
-
-    # D8-D9: Update and draw the dragon
-    # ---> TEST AFTER THESE LINES <--- #
-
-
-
-    # I7-I8: Update and draw the flame
-    # ---> TEST AFTER THESE LINES <--- #
-
-
-    # B4: Flip the display
-    # ---> TEST AFTER THIS LINE <--- #
+    # D8-D9:
+    dragon.draw()
+    # I7-I8:
+    if flame_on:
+        flame.draw()
+    # B4:
     pygame.display.flip()
 
 
     #### ---- TIME & SCORE ---- ####
 
-    # C5: Tick the clock with 30 framerate
-    # ---> TEST AFTER THIS LINE <--- #
+    # C5:
 
+    # O2:
 
-    # O2: Increment the distance traveled by 1
-
-
-    # Q2-Q3: Don't let the time between cloud spawns
-    # become less than 750 milliseconds
-    # ---> TEST AFTER THESE LINES <--- #
-
-
+    # Q2-Q3:
+    c.tick(30)
 
 
 #### ---- FINAL SCORE ---- ####
 
-# O3: Print the distance traveled / 100 as part of a
-# message to the user
-# ---> TEST AFTER THIS LINE <--- #
-
-
-# P3: Print the score as part of a message to the user
-# ---> TEST AFTER THIS LINE <--- #
+# O3:
+print("Balloons popped: " + str(score))
+# P3:
+print("Distance: " + str(distance/1000) + " meters")
